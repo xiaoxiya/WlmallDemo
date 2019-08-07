2bbd0ef46b9f523baa3abb2faa8086982acd1a77
xiaoxiya <1292733831@qq.com>
2019-08-07 15:39:47 +0800

    创建项目基本结构

---
+++ springboot-demo/wlmalldemo-web/src/test/java/com/xiaoxiya/wlmalldemoweb/WlmalldemowebApplicationTests.java
@@ -0,0 +1,16 @@
+package com.xiaoxiya.wlmalldemoweb;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WlmalldemowebApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
