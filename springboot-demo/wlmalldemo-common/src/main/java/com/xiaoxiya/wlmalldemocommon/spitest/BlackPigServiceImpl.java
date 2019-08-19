33ab065e480ac492b48c6d05c698087263d17f82
xiaoxiya <1292733831@qq.com>
2019-08-19 14:59:10 +0800

    Java SPi机制测试

---
+++ springboot-demo/wlmalldemo-common/src/main/java/com/xiaoxiya/wlmalldemocommon/spitest/BlackPigServiceImpl.java
@@ -0,0 +1,8 @@
+package com.xiaoxiya.wlmalldemocommon.spitest;
+
+/**
+ * @author luopeng
+ * @date 2019/8/13 11:35
+ */
+public class BlackPigServiceImpl {
+}
---
+++ springboot-demo/wlmalldemo-common/src/main/java/com/xiaoxiya/wlmalldemocommon/spitest/SpiTest.java
@@ -0,0 +1,8 @@
+package com.xiaoxiya.wlmalldemocommon.spitest;
+
+/**
+ * @author luopeng
+ * @date 2019/8/13 11:33
+ */
+public class SpiTest {
+}
